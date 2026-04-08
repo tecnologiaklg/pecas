3251689586a7c6ae7497cd48d6ba100980e78964
tecnologiaklg <tecnologia.klgdobrasil@gmail.com>
2026-04-08 15:24:20 -0300

    a

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -3,5 +3,8 @@ export const vendedoresLista = [
   { codigo: "55", nome: "Junior" },
   { codigo: "140", nome: "Duda" },
   { codigo: "154", nome: "João" },
-  { codigo: "155", nome: "Bryan" }
+  { codigo: "155", nome: "Bryan" },
+  { codigo: "159", nome: "Marilia" },
+  { codigo: "160", nome: "Samuel" },
+  { codigo: "161", nome: "Vinicius" },
 ];
